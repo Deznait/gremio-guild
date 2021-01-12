193d67c48bdeac6e54bf41832d1c3afeb9f57ac3
Alvaro Olea Rojas <alvaroolear@gmail.com>
2021-01-12 16:17:23 +0100

    First commit

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
-# gremio-guild
+# Gremio de Nordrassil - Site ![v0.1.0](https://img.shields.io/github/package-json/v/Deznait/gremio-guild.svg)
+
+This is a website built with [Vue.js](https://vuejs.org/) for the Gremio de Nordrassil WoW guild. It's a work in progress.
 
 ## Project setup
+Install dependencies
 ```
 npm install
 ```
 
-### Compiles and hot-reloads for development
+## Compiles and hot-reloads for development
 ```
 npm run serve
 ```
 
-### Compiles and minifies for production
+## Building for production
+A minified production build can be generated for deployment:
 ```
 npm run build
 ```
 
-### Lints and fixes files
+## Lints and fixes files
 ```
 npm run lint
 ```
 
-### Customize configuration
+## Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/main.js
+++ src/main.js
@@ -1,12 +1,43 @@
 import Vue from "vue";
+
+// BootstrapVue
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+
+// Vue Font-Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faUserSecret);
+
+// Custom directives
+import "./directives";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// Global CSS
+import "./assets/sass/main.sass";
+
 Vue.config.productionTip = false;
 
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+    router,
+    store,
+    FontAwesomeIcon,
+    methods: {
+        init() {
+            store.dispatch("oauth/getToken", null, { root: true })
+        }
+    },
+    created() {
+        this.init()
+        console.log("process.env:")
+        console.log(process.env)
+    },
+    render: h => h(App)
+}).$mount("#app")
